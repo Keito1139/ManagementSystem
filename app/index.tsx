c99f0cfe8c1ed16d46b2a05cf84dcdb0b5d864df
Keito1139 <kekeito2200102@icloud.com>
2024-10-06 17:15:50 +0900

    一旦表示は出来た

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../src/screens/Login'; // ログイン画面のインポート
@@ -7,11 +6,9 @@ const Stack = createStackNavigator();
 
 const Index = () => {
   return (
-    <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
-    </NavigationContainer>
   );
 };
 
